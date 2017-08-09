a38e267237c396c073195bb647920b7eeddd69e2
Robson Cavalcante <robsoncavalcante.es@gmail.com>
2017-08-08 22:58:19 -0300

    taking data from HTML page using JS

--- js/controllers/pagMainCtrl.js
+++ js/controllers/pagMainCtrl.js
@@ -3,33 +3,6 @@ angular.module("MyApp").controller("pagMainCtrl", function ($scope, $rootScope, 
     $scope.qtdTestCases = 0;
     $scope.arrayTituloTestesCases = null;
 
-    function readSingleFile(evt) {
-      console.log("chamou readSingleFile");
-
-              //Retrieve the first (and only!) File from the FileList object
-              var f = evt.target.files[0];
-              if (f) {
-                  var r = new FileReader();
-                  r.onload = function (e) {
-                      var contents = e.target.result;
-                      document.getElementById("ReadResult").innerHTML = contents;
-
-                    // var qtdTestCase = contents.getElementsByTagName('h3');
-                    // var testcases = 0;
-                    //
-                    // for (var i = 0; i < qtdTestCase.length; i++) {
-                    //     testcases++;
-                    //     var nametestecase = qtdTestCase.item(i);
-                    //     scope.arrayTituloTestesCases.push(testecase);
-                    // }
-                    // $scope.qtdTestCase = testecases;
-                    // alert("QTD testes cases is "+ testecase);
-                  }
-                  r.readAsText(f);
-              } else {
-                  alert("Failed to load file");
-              }
-          }
   })
   .controller('materializeCtrl', function ($scope) {
     $scope.openModal = function ($event) {
